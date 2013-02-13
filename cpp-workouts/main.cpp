9a19974a276ca8e8ccf9905ab5b6878f43949510
akorzec <alexander.korzec@gmail.com>
2013-02-12 22:26:44 -0800

    Initial Commit

---
+++ cpp-workouts/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  cpp-workouts
+//
+//  Created by Alexander Korzec on 2/12/13.
+//  Copyright (c) 2013 Fantasy Hip Hop. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
